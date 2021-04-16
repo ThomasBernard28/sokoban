cfc20fd1c895ef5c857e9989df2427fa7bb8603f
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2021-04-16 21:04:33 +0200

    Change some icons

--- src/main/java/be/ac/umons/Sokoban/JavaFX/Sprite/IconImg.java
+++ src/main/java/be/ac/umons/Sokoban/JavaFX/Sprite/IconImg.java
@@ -6,9 +6,9 @@ public enum IconImg {
     PLAY(7, 7),
     STOP(8, 2),
     EXIT(2, 8),
-    RELOAD(1, 4),
+    RELOAD(2, 4),
     SAVE(9, 3),
-    RESET(8, 8);
+    RESET(4, 2);
 
     private final static int cellSize = 50;
 
