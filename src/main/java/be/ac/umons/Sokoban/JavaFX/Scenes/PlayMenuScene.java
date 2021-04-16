cffbaa17da45986bfc329f1dc051e97ba662a78c
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2021-04-16 14:12:31 +0200

    nothing special

--- src/main/java/be/ac/umons/Sokoban/JavaFX/Scenes/PlayMenuScene.java
+++ src/main/java/be/ac/umons/Sokoban/JavaFX/Scenes/PlayMenuScene.java
@@ -38,33 +38,26 @@ public class PlayMenuScene extends SceneTool{
 
         StackPane LvlSelectButton = new StackPane();
         StackPane LoadSavedGameButton = new StackPane();
-        StackPane LvlGeneratorButton = new StackPane();
 
         ImageView LvlSelectButtonImg = SpriteUI.getUIImg(UIImg.YELLOW_BUTTON02);
         ImageView LoadSavedGameButtonImg = SpriteUI.getUIImg(UIImg.RED_BUTTON01);
-        ImageView LvlGeneratorButtonImg = SpriteUI.getUIImg(UIImg.YELLOW_BUTTON02);
 
         LvlSelectButtonImg.setScaleX(2);
         LvlSelectButtonImg.setScaleY(2);
         LoadSavedGameButtonImg.setScaleX(2);
         LoadSavedGameButtonImg.setScaleY(2);
-        LvlGeneratorButtonImg.setScaleX(2);
-        LvlGeneratorButtonImg.setScaleY(2);
+
 
         Label LvlSelectText = new Label("Level Selection");
-        Label LvlGeneratorText = new Label("Generate");
         Label LoadText = new Label("Load");
 
         LvlSelectText.setFont(Font.font("impact", 40));
-        LvlGeneratorText.setFont(Font.font("impact", 40));
         LoadText.setFont(Font.font("impact", 40));
 
         LvlSelectButton.getChildren().addAll(LvlSelectButtonImg, LvlSelectText);
-        LvlGeneratorButton.getChildren().addAll(LvlGeneratorButtonImg, LvlGeneratorText);
         LoadSavedGameButton.getChildren().addAll(LoadSavedGameButtonImg, LoadText);
 
         LvlSelectButton.setStyle("-fx-cursor: hand;");
-        LvlGeneratorButton.setStyle("-fx-cursor: hand;");
         LoadSavedGameButton.setStyle("-fx-cursor: hand;");
 
         LvlSelectButton.setOnMouseClicked(event -> {
@@ -72,16 +65,12 @@ public class PlayMenuScene extends SceneTool{
             SceneList.LVL_SELECTION.setOnActive();
         });
 
-        LvlGeneratorButton.setOnMouseClicked(event -> {
-            //TODO apply the Generator scene
-
-        });
 
         LoadSavedGameButton.setOnMouseClicked(event -> {
             LoadScene.makeScene();
             SceneList.LOAD.setOnActive();
         });
-        centerSide.getChildren().addAll(LvlSelectButton, LvlGeneratorButton, LoadSavedGameButton);
+        centerSide.getChildren().addAll(LvlSelectButton,LoadSavedGameButton);
         centerSide.setOrientation(Orientation.VERTICAL);
         centerSide.setAlignment(Pos.CENTER);
         centerSide.setVgap(100);
