8cdb4b192158538eb40a7646a69aed9db1a902e5
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2021-04-24 15:00:56 +0200

    Working unit test but still need to fix console call

---
+++ src/main/java/be/ac/umons/Sokoban/JavaFX/Scenes/StatScene.java
@@ -0,0 +1,10 @@
+package be.ac.umons.Sokoban.JavaFX.Scenes;
+
+import javafx.scene.layout.GridPane;
+
+public class StatScene extends SceneTool{
+
+    public GridPane statPane = new GridPane();
+
+
+}
